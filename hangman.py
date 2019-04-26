b06866b38aea13ed810d2b8618638db5845f2f96
samccain <samuel.mccain@accenturefederal.com>
2019-04-26 11:42:46 -0400

    working features: directory/file checking and creation/downloading, reading the file, and selecting a random word of at least a minimum length

--- hangman.py
+++ hangman.py
@@ -1,34 +1,97 @@
 import urllib.request
 import shutil
 import random
+import pathlib
 
 class Hangman():
     def __init__(self):
-        self.wordLength = 12     # minimum length of word chosen by game
+        self.wordLength = 12    # minimum length of word chosen by game
         self.word = str()       # word chosen by game
         self.gamestate = 0      # whether game is in progress or not
         self.stateHM = 0        # state of the hangman; 1-6
-        self.guess = str()
+        self.guess = str()      # input from player; 1 alphabetical character
+        self.lines = list()     # list of words read from text file
 
-    def wordPick(self):
+    def openFile(self):
+        # This method does multiple things. First, it checks if the ./word_data/ directory exists and creates it if it
+        # does not exist. Second, it checks if the word.txt file exists in the directory and downloads it if it does
+        # not exist. Third, it opens the file and writes the contents to a list.
 
-        fileName = 'sowpods.txt'
+        # checking and creating the directory if necessary
+        pathlib.Path('./word_data/').mkdir(parents=True, exist_ok=True)
+
+        # storing the relative file path to the words.txt file
+        filePath = './word_data/words.txt'
 
         try:
-            data = open(fileName)
+            # checking if the file exists
+            with open(filePath) as f:
+                print('file exists')
         except FileNotFoundError:
+            # downloading the file to the directory if it does not exist
+            print('file does not exist')
             url = 'http://norvig.com/ngrams/sowpods.txt'
-            with urllib.request.urlopen(url) as response, open(fileName, 'wb') as out_file:
-                shutil.copyfileobj(response, out_file)
+            urllib.request.urlretrieve(url, filePath)
+            print('file downloaded to ' + filePath)
         finally:
-            with open(fileName) as f:
-                lines = f.readlines()
+            # storing the file contents as a list
+            with open(filePath) as f:
+                self.lines = f.readlines()
+            print('file read')
 
-                while len(self.word) < self.wordLength:
-                    self.word = random.choice(lines)
+    def wordPick(self):
+        # This method randomly selects a word of a minimum length from the stored list
+        while len(self.word) < self.wordLength:
+            self.word = random.choice(self.lines)
 
+        print(self.word)
 
+    def 
+
+    def printhangman(self, wrongGuesses):
+        # This method prints on of the 7 states of the hangman
+        if wrongGuesses == 0:
+            print('|‾‾‾‾‾‾‾|')
+            print('|')
+            print('|')
+            print('|')
+            print('|')
+        elif wrongGuesses == 1:
+            print('|‾‾‾‾‾‾‾|')
+            print('|     O')
+            print('|')
+            print('|')
+            print('|')
+        elif wrongGuesses == 2:
+            print('|‾‾‾‾‾‾‾|')
+            print('|     O')
+            print('|     |')
+            print('|     ')
+            print('|')
+        elif wrongGuesses == 3:
+            print('|‾‾‾‾‾‾‾|')
+            print('|     O')
+            print('|    /|')
+            print('|     ')
+            print('|')
+        elif wrongGuesses == 4:
+            print('|‾‾‾‾‾‾‾|')
+            print('|     O')
+            print('|    /|\\')
+            print('|     ')
+            print('|')
+        elif wrongGuesses == 5:
+            print('|‾‾‾‾‾‾‾|')
+            print('|     O')
+            print('|    /|\\')
+            print('|    /')
+            print('|')
+        elif wrongGuesses == 6:
+            print('|‾‾‾‾‾‾‾|')
+            print('|     O')
+            print('|    /|\\')
+            print('|    / \\')
+            print('|')
 
 
 game = Hangman()
-game.wordPick()
